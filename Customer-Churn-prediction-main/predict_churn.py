5a54b8c786210811c3bca3d04c995a844520b05a
Haritha <haritha6727@gmail.com>
2025-09-21 11:52:17 +0530

    Add files via upload

---
+++ Customer-Churn-prediction-main/predict_churn.py
@@ -0,0 +1,25 @@
+
+
+import pandas as pd
+import joblib
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+
+
+model = joblib.load(r'C:\Users\user\Downloads\Customer-Churn-prediction-main\churn_model.pkl')
+
+
+def predict_churn(input_dict):
+    
+    input_df = pd.DataFrame([input_dict])
+
+
+    le = LabelEncoder()
+
+    
+    for col in input_df.select_dtypes(include='object').columns:
+        input_df[col] = le.fit_transform(input_df[col])
+
+    
+    churn_prob = model.predict_proba(input_df)[:, 1][0]
+    return churn_prob
